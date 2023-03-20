b9cc9a0fdf6a9b9d43478a38b842207009876a85
Dmitry Sergeev <dmitrijsergeev@gmail.com>
2023-03-20 08:26:15 +0400

    a bit change

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -4,7 +4,7 @@ import s from 'src/components/Profile/Profile.module.css'
 export const Profile = () => {
     return (
         <div>
-
+            <h3>That's my profile</h3>
         </div>
     );
 };
